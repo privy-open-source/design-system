ac9f27b7e2ebde85b1da465f13b12da13032b77d
adenvt <adenvt@users.noreply.github.com>
2022-05-28 03:17:08 +0000

    deploy: 6759953cc1643f79fd776fcc17455729899f6946

--- assets/chunks/use-loading.67d52482.js
+++
@@ -1,1 +0,0 @@
-import{t as r}from"./index.b13696b4.js";import{r as l,c as d}from"../app.c3e9a2ef.js";function f(e={elapsed:!0}){const a=l(0),t=l(e.elapsed!==!1),u=d({get(){return a.value>0||t.value},set(s){s===!0?a.value++:a.value--}});return r(()=>{if(e.elapsed!==!1){const s=e.elapsed===!0?200:e.elapsed;setTimeout(()=>{t.value=!1},s)}}),u}export{f as u};
