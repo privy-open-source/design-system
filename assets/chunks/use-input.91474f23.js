fb1414b9724b5d3bc457918d5aeb3d05c49dd508
adenvt <adenvt@users.noreply.github.com>
2022-08-31 09:38:02 +0000

    deploy: 0f1c52e56d928bc5ea22c329ece2138047452564

---
+++ assets/chunks/use-input.91474f23.js
@@ -0,0 +1,1 @@
+import{s}from"./index.7a72f317.js";import{r as u,c as l,C as n}from"../app.0224d108.js";function d(e){const t=u(e.modelValue),{emit:o}=n(),a=l({get(){return e.modelValue},set(r){o("update:modelValue",r)}});return s(t,a),t}export{d as u};
