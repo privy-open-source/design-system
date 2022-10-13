ea20878280ba265da4f32e88f79e1a7b6b38fbbd
adenvt <adenvt@users.noreply.github.com>
2022-10-13 03:07:09 +0000

    deploy: 0c3560bad7b05481c96b8b8ecb8a8e6486f31038

---
+++ assets/chunks/Badge.3a053e23.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./Badge.vue_vue_type_style_index_0_lang.4634fa6a.js";import{_ as e,o,c as r,r as t,n}from"../app.14fdb4ab.js";function c(s,l,p,d,m,_){return o(),r("span",{"data-testid":"badge",class:n(s.classNames)},[t(s.$slots,"default")],2)}const $=e(a,[["render",c]]);export{$ as B};
