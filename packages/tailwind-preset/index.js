1b8f95fe3f7794da2e9e8b72d1787e5221ebe7ad
Avriqq <friqz_supermind@yahoo.co.id>
2022-12-05 17:37:41 +0700

    deleted: remove boxShadow in tailwind config preset

--- packages/tailwind-preset/index.js
+++ packages/tailwind-preset/index.js
@@ -449,18 +449,6 @@ module.exports = {
       '2xl'    : '22px',
       'full'   : '999999px',
     },
-    boxShadow: {
-      'sm'        : '0 1px 2px rgba(var(--tw-shadow-color, 0, 0, 0), 0.05)',
-      'DEFAULT'   : '0 1px 3px rgba(var(--tw-shadow-color, 0, 0, 0), 0.1), 0 1px 2px rgba(var(--tw-shadow-color, 0, 0, 0), 0.06)',
-      'md'        : '0 4px 6px -1px rgba(var(--tw-shadow-color, 0, 0, 0), 0.1), 0 2px 4px -1px rgba(var(--tw-shadow-color, 0, 0, 0), 0.06)',
-      'lg'        : '0 10px 15px -3px rgba(0,0,0, 0.1), 0 4px 6px -2px rgba(var(--tw-shadow-color, 0, 0, 0), 0.05)',
-      'xl'        : '0 0 1px rgba(var(--tw-shadow-color, 0, 0, 0), 0.15), 0 8px 10px -6px rgba(var(--tw-shadow-color, 0, 0, 0), 0.1), 0 20px 25px -5px rgba(var(--tw-shadow-color, 0, 0, 0), 0.1)',
-      '2xl'       : '0 25px 50px -12px rgba(var(--tw-shadow-color, 0, 0, 0), 0.25)',
-      'inner'     : 'inset 2px 0 4px rgba(var(--tw-shadow-color, 0, 0, 0), 0.06), inset 0 4px 6px -1px rgba(var(--tw-shadow-color, 0, 0, 0), 0.06)',
-      'none'      : 'none',
-      'outline'   : '0 0 0 4px rgba(var(--tw-shadow-color, 220, 220, 220), 0.35)',
-      'onemphasis': '0 10px 15px -3px rgba(var(--tw-shadow-color, 220, 220, 220), 0.35)',
-    },
     opacity: {
       0  : 0,
       5  : 0.05,
