be51390ff832d72ff89d700b8092ea723c39eaaa
adenvt <adenvt@users.noreply.github.com>
2022-12-17 10:45:55 +0000

    deploy: f1f5a1c73e2a2f664463578e634cfb1bebfd4384

---
+++ assets/chunks/Button.f89b13a0.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./Button.vue_vue_type_style_index_0_lang.8abde2d4.js";import{f as a,o as e,c as o,g as r,n}from"../app.12128179.js";function c(s,p,l,f,m,d){return e(),o("button",{"data-testid":"btn",class:n(s.classNames)},[r(s.$slots,"default")],2)}const u=a(t,[["render",c]]);export{u as p};
