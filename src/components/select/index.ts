c5bcf251eef2ec42461a30eedf4889b23b1cc6a7
Ade Novid <adenvt@gmail.com>
2023-07-20 11:09:16 +0700

    fix(select): allow using component without v-model

--- src/components/select/index.ts
+++ src/components/select/index.ts
@@ -1,3 +1,5 @@
+import { isEqual } from '../utils/value'
+
 export interface SelectItem {
   text: string,
   value: unknown,
@@ -11,4 +13,9 @@ export interface SelectProps {
 
 export function defineOptions (options: SelectItem[]): SelectItem[] {
   return options
+}
+
+export function findSelected (items: SelectItem[], value: unknown): SelectItem {
+  return items.find((item) => isEqual(item.value, value))
+    ?? { text: '', value: undefined }
 }
