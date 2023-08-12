27dda84163e2bf3fb67426b604dd28eec5bf1738
adenvt <adenvt@users.noreply.github.com>
2023-08-12 00:44:18 +0000

    chore(persona-icon): sync with figma

---
+++ packages/persona-icon/vue/path/32.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+    fill="none"
+    class="persona-icon"
+    viewBox="0 0 32 32"><path
+      fill="currentColor"
+      d="M18.845 6.038a1 1 0 0 1 .687 1.237l-5.143 18a1 1 0 1 1-1.923-.55l5.143-18a1 1 0 0 1 1.236-.686Z" /></svg>
+</template>
