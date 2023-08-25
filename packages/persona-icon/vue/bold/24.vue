d1aad1200a41ee2e7f525a104941d108aa5edd28
Ade Novid <adenvt@gmail.com>
2023-08-25 13:55:49 +0700

    chore(persona-icon): sync with figma

--- packages/persona-icon/vue/bold/24.vue
+++ packages/persona-icon/vue/bold/24.vue
@@ -8,6 +8,6 @@
     viewBox="0 0 24 24"><path
       fill="currentColor"
       fill-rule="evenodd"
-      d="M7 4a1.5 1.5 0 0 0-1.5 1.5v13A1.5 1.5 0 0 0 7 20h6.5a5 5 0 0 0 2.457-9.356A4.5 4.5 0 0 0 12 4H7Zm5 6H8.5V7H12a1.5 1.5 0 0 1 0 3Zm-3.5 3v4h5a2 2 0 1 0 0-4h-5Z"
+      d="M7 4a1.5 1.5 0 0 0-1.5 1.5v13A1.5 1.5 0 0 0 7 20h6.5a5 5 0 0 0 2.457-9.356A4.5 4.5 0 0 0 12 4H7Zm5 6H8.5V7H12a1.5 1.5 0 0 1 0 3Zm-3.5 3h5a2 2 0 1 1 0 4h-5v-4Z"
       clip-rule="evenodd" /></svg>
 </template>
