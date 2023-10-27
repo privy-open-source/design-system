6fdc2ab1f34453b7e48247f73a3620604118d293
Ade Novid <adenvt@gmail.com>
2023-10-27 13:37:37 +0700

    fix(persona): fix font bold

--- packages/persona/src/module.ts
+++ packages/persona/src/module.ts
@@ -41,7 +41,7 @@ export default defineNuxtModule<ModuleOptions>({
           href       : 'https://fonts.gstatic.com',
           crossorigin: '',
         },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;1,400&display=swap' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,400;0,9..40,500;0,9..40,700;1,9..40,400&display=swap' },
       )
     }
 
